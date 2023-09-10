37396c5dbaa29a2f2b989e4f7aff19c6915f1c88
ibi111 <ibtsamsadiqaim@gmail.com>
2023-09-10 22:07:13 +0500

    all helper functions added

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1,7 +1,9 @@
 import socket
+import sys
 import re
 
 BUFFER_SIZE=100000
+
 
 def getContentLength(Host,directory):
     """
@@ -9,22 +11,74 @@ def getContentLength(Host,directory):
     :param directory: location of the resource
     :return: length of content to be downloaded
     """
-    contentLength=0
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketHeader:
-        requestHeader="HEAD "+directory+" HTTP/1.1\r\nHost:"+Host+":80\r\n\r\n"
-        print(requestHeader)
-        socketHeader.connect((Host,80))
-        socketHeader.sendall(requestHeader.encode('utf-8'))
+    socketHeader=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    requestHeader="HEAD "+directory+" HTTP/1.1\r\nHost:"+Host+":80\r\n\r\n"
 
-        data = socketHeader.recv(BUFFER_SIZE)
-        text = data.decode()
-        print(text)
+    socketHeader.connect((Host,80))
 
-        # contentLength = re.split('Content-Length:',text)[-1]
-        # contentLength=int(contentLength.splitlines()[0])
+    socketHeader.send(requestHeader.encode('utf-8'))
 
-    return 0
+    data = socketHeader.recv(100000)
 
-def tester():
-    print(getContentLength("filesamples.com","/samples/document/txt/sample3.txt"))
-tester()
+    text = data.decode()
+
+    contentLength = re.split('Content-Length:',text)[-1]
+
+    contentLength=int(contentLength.splitlines()[0])
+
+    socketHeader.close()
+
+    return contentLength
+def readCommandLineArguments():
+    if len(sys.argv) == 10:
+        if sys.argv[1] == '-r':
+            resume_status=True
+
+        num_of_connections=int(sys.argv[3])
+        time=int(sys.argv[5])
+        Host=str(sys.argv[7].split("/", 1)[0])
+        Directory="/"+str(sys.argv[7].split("/", 1)[1])
+        FilePathToDownloadTo=str(sys.argv[9])
+
+        return resume_status,num_of_connections,time,Host,Directory,FilePathToDownloadTo
+
+    elif len(sys.argv) == 9:
+        resume_status=False
+        num_of_connections=int(sys.argv[2])
+        time=int(sys.argv[4])
+        Host=str(sys.argv[6].split("/", 1)[0])
+        Directory="/"+str(sys.argv[6].split("/", 1)[1])
+        FilePathToDownloadTo=str(sys.argv[8])
+
+        return resume_status,num_of_connections,time,Host,Directory,FilePathToDownloadTo
+
+    else:
+        print("Command line arguments not right")
+        sys.exit(0)
+
+def combineData(NUMBER_OF_THREADS,RESUMING_STATUS,FILE_LOCATION):
+    """
+
+    :param NUMBER_OF_THREADS: number of  threads downloading the file
+    :param RESUMING_STATUS: resumed or not
+    :param FILE_LOCATION: location of downloaded pieces
+    :return:
+    """
+
+    print("\n\nFile is downloaded only writing\n\n")
+
+    arrayOfFiles=[]
+
+    for i in range(NUMBER_OF_THREADS):
+        arrayOfFiles.append('C:\\Users\\ibi\\Desktop\\cnProject\\test'+str(i+1)+'.txt')
+
+    with open(str(FILE_LOCATION)+'\\testAll.txt', 'wb+') as outfile:
+
+        if RESUMING_STATUS==False:
+            outfile.truncate(0)
+
+        for fname in arrayOfFiles:
+            with open(fname) as infile:
+                outfile.write(infile.read().encode('utf-8'))
+
+    print("WRITTEN")
